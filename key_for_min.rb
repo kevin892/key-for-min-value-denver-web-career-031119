901a4ba8c5488cf691996a1a3db6c8099aef9dd2
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-30 21:49:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
  low_age = nil
  low_name = nil
  name_hash.each do |name, age|
-    if low_age == nil || low_age > age
+    if low_age == nil || low_age < age
       low_age = age
       low_name = name
     end
